78c005c2b09bdab053dbcf8ed549ff9ca5747325
You <thibaultyou@gmail.com>
2019-02-28 20:27:59 +0100

    fix: Fixing path in install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip3 install --upgrade pip
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-pip3 install -r $DIR/rasa_fasttext/requirements.txt
+pip3 install -r $DIR/rasa/requirements.txt
 sh $DIR/rasa/download.sh fr https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.fr.300.vec.gz
 sh $DIR/rasa/generate.sh fr
